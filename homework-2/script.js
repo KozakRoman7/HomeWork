7434e5bef390367769a15a5892a407d2051e81b5
Roma <roman.kozak2002@gmail.com>
2022-02-23 22:40:25 +0300

    fix

--- homework-2/script.js
+++ homework-2/script.js
@@ -2,27 +2,29 @@ let n = +prompt('Введіть число "N":');
 let m = +prompt('Введіть число "M":');
 let EvenOrOdd = '';
 let indicator = 0;
- 
+
+
 if (Number.isInteger(n) && Number.isInteger(m) && n < m){
    EvenOrOdd = confirm('Пропускати парні числа?')
 } else {
    alert('Будь ласка, повторіть спробу користуючись тільки цілими числами і умовою N < M!');
 }
 
-if (EvenOrOdd){
-   for (let i=n; i<=m; i++){
-      indicator += i;
-   } alert(indicator);
-} else {
+
+if (EvenOrOdd) {
    for(let i=n; i<=m; i++){
       if (i % 2 !== 0){
+         indicator += i;
+      } 
+   } 
+   alert(indicator);
+} 
+else {
+   for(let i=n; i<=m; i++){
          indicator +=i;
-      }
-   } alert(indicator);
-}
+    }alert(indicator);
+ } 
 alert('Щоб спробувати ще раз - обновіть сторіку.');
-
-
 
 
 
